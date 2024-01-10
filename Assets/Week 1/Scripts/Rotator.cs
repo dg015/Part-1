307a2fa5138172c247ab43d1b73ba3a32680869d
dg015 <bruno.barbosa8@yahoo.com>
2024-01-10 14:12:44 -0500

    Task 1 complete: clock and moving hands are functionning.

---
+++ Assets/Week 1/Scripts/Rotator.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Rotator : MonoBehaviour
+{
+     [SerializeField] private float speed = 30.0f;
+     
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        rotation();
+    }
+
+    void rotation()
+    {
+        transform.Rotate(Vector3.forward, - speed  * Time.deltaTime);
+
+    }
+}
