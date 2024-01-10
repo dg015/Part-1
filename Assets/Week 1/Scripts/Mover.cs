1171468e100a9e9bdef2bce9ed6398e6b2b3cd7b
dg015 <bruno.barbosa8@yahoo.com>
2024-01-10 15:17:27 -0500

    added functionality to tank

--- Assets/Week 1/Scripts/Mover.cs
+++ Assets/Week 1/Scripts/Mover.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
-
+   [SerializeField] GameObject missilePrefab;
     [SerializeField] private float speed = 3f;
+    [SerializeField] private Transform barrel;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,5 +19,10 @@ public class Mover : MonoBehaviour
     {
         float direction = Input.GetAxis("Horizontal");
        transform.Translate(speed* direction * Time.deltaTime,0,0);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(missilePrefab,barrel.position, barrel.rotation);
+        }
     }
 }
