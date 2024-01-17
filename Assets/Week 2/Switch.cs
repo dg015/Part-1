915e99fa435c83899c60a183f5142dafe6c2839a
dgo015 <bruno.barbosa8@yahoo.com>
2024-01-17 15:47:58 -0500

    Adding switch code from class

---
+++ Assets/Week 2/Switch.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : MonoBehaviour
+{
+    SpriteRenderer spriteRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("trigger");
+        spriteRenderer.color = Color.red;
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        spriteRenderer.color = Color.green;
+    }
+}
