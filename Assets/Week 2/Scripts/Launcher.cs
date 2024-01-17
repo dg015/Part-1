c66849c4b8ce905810648c633a1b00fd27fec679
dg015 <bruno.barbosa8@yahoo.com>
2024-01-17 17:18:36 -0500

    Task 4a completed

---
+++ Assets/Week 2/Scripts/Launcher.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Launcher : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] GameObject missile;
+    [SerializeField] Transform spawn;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Instantiate(missile,spawn.position,spawn.rotation);
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Destroy(missile);
+    }
+}
