0999a19595402c34c1f943410b6947417411d416
rodbruno <bruno.barbosa8@yahoo.com>
2024-01-24 14:28:46 -0500

    Added functionality to car

---
+++ Assets/Week 3/Scripts/Car.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    float acceleration;
+    float sterring;
+    [SerializeField] float fowardSpeed = 500f;
+    [SerializeField] float steeringSpeed = 100f;
+    Rigidbody2D rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        acceleration = Input.GetAxis("Vertical");
+        sterring = Input.GetAxis("Horizontal");
+
+    }
+    private void FixedUpdate()
+    {
+      rigidbody.AddTorque(-sterring * steeringSpeed * Time.deltaTime);  
+      Vector2 force = transform.up * acceleration * fowardSpeed * Time.deltaTime;
+        rigidbody.AddForce(force);
+    }
+}
