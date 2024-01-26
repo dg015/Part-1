4e749e01a356ec6a571cab8535232f485790b1cd
dg015 <bruno.barbosa8@yahoo.com>
2024-01-26 16:41:32 -0500

    Added script death ray that instantly kills player upon touching

--- Assets/Assignment/Scripts/EnemyType2.cs
+++ Assets/Assignment/Scripts/EnemyType2.cs
@@ -22,31 +22,16 @@ public class EnemyType2 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        searchMode();
+        spin();
     }
 
-    private void searchMode()
-    {
-        
-        if (detection.GetComponent<DetectionArea>().detected == true)
-        {
-            timer();
-            if (ready)
-            {
-                AttackMode();
-            }
-        }
-        else
-        {
-            spin();
-        }
 
-
-    }
+    
 
 
     private void spin()
     {
+
         float time =+ Time.deltaTime * speed;
        
         transform.Rotate(new Vector3(0, 0, time));
@@ -55,40 +40,5 @@ public class EnemyType2 : MonoBehaviour
 
     }
 
-    private void timer()
-    {
-        if (time > 0)
-        {
-            time -= Time.deltaTime;
-        }
-        else if (time < 0)
-        {
-            time = 1f;
-            ready = true;
-        }
-
-    }
-
-    private void AttackMode()
-    {
-        ready = false;
-
-        Debug.Log("found enemy");
-        //barrel.LookAt(target.position);
-
-
-        Vector2 dir = target.position - transform.position;
-        float ang = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-        // Get the angle in radiants of the direction the enemy to the player 
-        //than gets the exact quadrom and later converts it into degrees
-        ang = ang - 90;
-        transform.rotation = Quaternion.Euler(0, 0, ang);
-        Instantiate(bullet, barrel.position, barrel.rotation);
-
-
-        //transform.Rotate(dir);
-        //transform.rotation = Quaternion.LookRotation(dir);
-
-
-    }
+    
 }
--- Assets/Week 1/Scripts/Traveler.cs
+++ Assets/Week 1/Scripts/Traveler.cs
@@ -22,4 +22,6 @@ public class Traveler : MonoBehaviour
     {
         Destroy(this.gameObject);
     }
+
+
 }
